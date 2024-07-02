f33c5b439376c470f1d5d893feedb7e11ee77949
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-07-02 15:01:10 -0300

    Básico sobre sentenças

---
+++ Fundamentos/sentença_2.js
@@ -0,0 +1,4 @@
+// operações simples podem só estar no console.log e a operação é realizada sem problemas
+console.log(1 + 3);
+// em caso de texto é necessário colocar aspas simples ou duplas
+console.log("Exemplo de texto");
