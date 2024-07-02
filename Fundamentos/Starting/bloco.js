2dafe078dda865fb819090578a95a0691f9eda3a
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-07-02 15:53:24 -0300

    Novas aulas

---
+++ Fundamentos/Starting/bloco.js
@@ -0,0 +1,14 @@
+// o bloco de código é delimitado pelo uso de {}
+// Bloco 1
+{
+    console.log("Bloco 1");
+    console.log("Passo 1");
+    console.log("Passo 2");
+    console.log();
+}
+// Bloco 2
+{
+    console.log("Bloco 2")
+    console.log("Passo 3")
+    console.log("Passo 4")
+}
