9d59b6268a810b7ff5c03f8d3c43a5430acce09d
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-07-02 14:53:05 -0300

    Pasta fundamentos e primeiro exemplo

---
+++ Fundamentos/sentença.js
@@ -0,0 +1,11 @@
+// o ponto e virgula é opcional no Javascript
+console.log("Olá mundo")
+
+// O bloco de código pode ser quebrado, apesar de não ser uma prática muito boa
+console
+    .log("Este é um código ruim");
+
+// a ordem normal dos comandos é rodar de cima para baixo
+console.log("Bom dia");
+console.log("Boa tarde");
+console.log("Boa noite");
