be1852518f318878781d08557898d8aae568c4bc
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-07-02 15:18:06 -0300

    Reorganizando pastas

---
+++ Fundamentos/Starting/comentario.js
@@ -0,0 +1,8 @@
+/* comentários são usados para documentar melhor o código
+ * os comentários também são úteis para desabilitar linhas específicas sem precisar excluir
+ * os comentários de multiplas linhas são usados nesse formato
+*/
+
+console.log("Bom dia")
+// console.log("Boa tarde")
+console.log("Boa noite") // o comentário começa depois do "//" 
